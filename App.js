6a047059158db4b2b14dc4077ec3fcb12e6eb9af
Victor Jann <victorjann@gmail.com>
2019-12-30 20:22:40 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,143 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import ImagePicker from 'react-native-image-crop-picker';
+import firebase from 'react-native-firebase';
+import { useForm } from 'react-hook-form'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  AsyncStorage,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  UserInfoForm: {screen: UserForm},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
+
+const { app } = firebase.storage();
+
+class HomeScreen extends Component<{}> {
+  state = {progress:0, showEmailPrompt:true, numPhotosTotal:0, numPhotosDone:0}
+
+  uploadImage = (image_array) => {
+    this.setState({numPhotosTotal:image_array.length, showEmailPrompt:true});
+    console.log("uploadImage method called");
+    for (let i = 0; i <image_array.length; i ++) {
+      this.setState({})
+      image = image_array[i];
+      console.log('got to uploadImage', i);
+      console.log(image.filename);
+      if (image.filename == undefined) {
+        continue;
+      }
+      const filename = image.filename; // Generate unique name
+      firebase
+        .storage()
+        .ref(`tutorials/images/${filename}`)
+        .putFile(image.path)
+        .on(
+          firebase.storage.TaskEvent.STATE_CHANGED,
+          snapshot => {
+            let state = {};
+            state = {
+              ...state,
+              progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // Calculate progress percentage
+            };
+            if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
+              AsyncStorage.setItem('images', JSON.stringify(image));
+              console.log("Uploaded: ", filename);
+              this.setState({numPhotosDone: this.numPhotosDone + 1});
+            }
+            this.setState(state);
+          },
+          error => {
+            unsubscribe();
+            alert('Sorry, Try again.');
+          }
+        );
+      }
+  }
+  imgPicker = () => {
+    try {
+      ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        multiple: true
+      }).then(images => {
+        // console.log(images);
+        if (images.length != 0) {
+          this.uploadImage(images);
+        }
+      });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+  handleEmailInput = (email : string) => {
+    console.log("email");
+    console.log(email);
+  }
+  closeDialog = () => {
+    //TODO: check if email is inputted
+    this.setState({showEmailPrompt:false});
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>
+        Current Picture {this.state.progress} % uploaded, {this.state.numPhotosTotal - this.state.numPhotosDone} left
+        </Text>
+
+
+        <TouchableOpacity style={{backgroundColor:'green', padding:10, margin:10}} onPress={this.imgPicker}>
+          <Text>Select Image!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+class UserForm extends Component<{}> {
+  render(){
+    <Text>uwu</Text>
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.lighter,
+  }
+});
---
+++ EmailDialog.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import firebase from 'react-native-firebase';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  AsyncStorage,
+  Image,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const { app } = firebase.storage();
+
+export default class EmailDialog extends Component<{}> {
+  render(){
+    return (
+      <View>
+        <Dialog.Container visible={true}>
+          <Dialog.Title>Upload Project</Dialog.Title>
+          <Dialog.Description>
+            Please enter your email to continue.
+          </Dialog.Description>
+          <Dialog.Button label="Cancel" />
+          <Dialog.Button label="Delete" />
+        </Dialog.Container>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.lighter,
+  }
+});
